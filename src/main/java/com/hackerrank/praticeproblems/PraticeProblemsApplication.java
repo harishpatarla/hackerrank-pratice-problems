2f7374b1b6c4c3f0a5fac3ed7f05d7a38016ab87
patharh <Harish.Patharla@ig.com>
2020-10-22 22:46:28 +0530

    Initial commit

---
+++ src/main/java/com/hackerrank/praticeproblems/PraticeProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.hackerrank.praticeproblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticeProblemsApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(PraticeProblemsApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/hackerrank/praticeproblems/PraticeProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerrank.praticeproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticeProblemsApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
